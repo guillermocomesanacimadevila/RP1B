a8acafb9786ce51aeaa0b54e66ece58f13fede94
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-03-28 13:55:43 +0000

    Update 04_parse_susceptible_vcfs.sh

--- Pre-processing/04_parse_susceptible_vcfs.sh
+++ Pre-processing/04_parse_susceptible_vcfs.sh
@@ -7,7 +7,7 @@
 BASE_URL="https://ftp.ebi.ac.uk/pub/databases/cryptic/release_june2022/reproducibility/"
 
 # CSV file containing the VCF column
-CSV_FILE="susceptible_samples.csv"
+CSV_FILE="susceptible_metadata.csv"
 
 # Extract VCF filenames and download each file
 while IFS=',' read -r _ _ VCF _; do
