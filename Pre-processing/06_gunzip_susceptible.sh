f8053215b9e35a7c9c3660f1e4e2c485f02626a4
Guillermo Comesaña Cimadevila <comesanaguillermo@gmail.com>
2025-03-28 13:58:09 +0000

    Update 06_gunzip_susceptible.sh

--- Pre-processing/06_gunzip_susceptible.sh
+++ Pre-processing/06_gunzip_susceptible.sh
@@ -4,9 +4,9 @@
 mkdir -p gunziped_susceptible
 
 # Loop through all .vcf.gz files
+# Keep the original .vcf.gz just in case | parse .vcf -> new dir
 for file in *.vcf.gz; do
     if [[ -f "$file" ]]; then
-        # Gunzip the file (keeps the original name but without .gz)
         gunzip -c "$file" > "gunziped_susceptible/${file%.gz}"
         echo "Uncompressed: $file -> gunziped_susceptible/${file%.gz}"
     fi
